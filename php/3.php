637363d939b5916f71c51b2af1ef54cab9a86642
Md Asif Iqbal <phpkolkata@gmail.com>
2022-04-30 09:05:39 +0530

    php variable

---
+++ php/3.php
@@ -0,0 +1,43 @@
+<?php
+
+// variable
+$x = 10;
+$y = 20;
+$z = $x + $y;
+
+print $z;
+print 'xyz';
+
+// install apache server in your mind
+
+
+// other programming language style
+// int x;
+// string y;
+
+// x = 10;
+// y = "abc";
+// x = "a"; // error
+
+
+// php way (auto type casting)
+$x = 10; // integer  type
+$x = "abc"; // string type
+
+
+// string = 'abc'
+// int = 1;
+// float = 1.00
+// double = 1.000000
+// boolean = true/false  , 0,1 , yes/no
+// null = emty itself
+
+
+// String
+// Integer
+// Float (floating point numbers - also called double)
+// Boolean
+// Array
+// Object
+// NULL
+// Resource
---
+++ php/4.php
@@ -0,0 +1,5 @@
+<?php
+$x = 10;
+$x = "abc10"; //string
+$y = 20; // integer
+$z = $x + $y;
