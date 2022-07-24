4f8daa9a8c6de81398c4156135a07b05939d4244
Md Asif Iqbal <phpkolkata@gmail.com>
2022-07-24 08:38:00 +0530

    lara-routes

--- myLara/routes/web.php
+++ myLara/routes/web.php
@@ -17,12 +17,34 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+$posts = [
+    1=>[
+        'title'=>'My Post 1',
+        'content'=> 'this is post 1 content'
+    ],
+    2=>[
+        'title'=>'My Post 2',
+        'content'=> 'this is post 2 content'
+    ],
+    3=>[
+        'title'=>'My Post 3',
+        'content'=> 'this is post 3 content'
+    ]
+];
+
 Route::get('/',function(){
-    // return "Home Page";
    return view('index');
-});
+})->name('home');
 
 Route::get('/page1',function(){
-    // return "Page1";
-   return view('page1');
-});
+   return view('pages.page1');
+})->name('pages.page1');
+
+Route::get('/page2',function(){
+   return view('pages.page2');
+})->name('pages.page2');
+
+Route::get('posts/{id?}', function($id=1) use($posts){
+    $data = $posts[$id];
+    return view('posts.show',['data'=>$data]);
+})->where('id','[0-9]+');
